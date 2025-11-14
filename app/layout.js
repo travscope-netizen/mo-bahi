ba3d0771658bf34ce617e96c9fdf5fc310fbc5b4
travscope-netizen <travscope@gmail.com>
2025-11-14 20:15:20 +0530

    Wrap children in CartProvider in RootLayout

--- app/layout.js
+++ app/layout.js
@@ -44,7 +44,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+  <CartProvider>
+    {children}
+  </CartProvider>
+</body>
+
     </html>
   );
 }
