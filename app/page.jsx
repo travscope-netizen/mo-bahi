830e504f2b04f3b6eb29e2f75c73d6004df74aae
travscope-netizen <travscope@gmail.com>
2025-11-14 19:15:07 +0530

    Add HomePage component with ProductList

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import ProductList from "../components/ProductList";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Featured Books</h1>
+      <ProductList />
+    </div>
+  );
+}
